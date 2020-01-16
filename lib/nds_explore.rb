e5a5e60ae0351f18bfe0366bb71de52bd2eee02f
Paul Kim <pkim1295@gmail.com>
2020-01-16 02:32:00 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -7,16 +7,17 @@ def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   pp nds
 end
-pp directors_database
 
 def print_first_directors_movie_titles
   director = directors_database
+  string = ""
+  num = 0
 
-  director[0][:movies][0].each_key do |key|
-    if director[0][:movies][0][key] == director[0][:movies][0][:title]
-      puts "#{director[0][:movies][0][key]}\n"
-    end
+  while num < director[0][:movies].length do
+    string += director[0][:movies][num][:title]
+    num += 1
   end
+  string
 end
 
 puts directors_database[0][:movies]
