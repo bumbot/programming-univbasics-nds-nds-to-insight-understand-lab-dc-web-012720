52b4c1195756100fae1ae31c5682bb5f73b620b9
Paul Kim <pkim1295@gmail.com>
2020-01-16 01:56:14 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,12 +12,12 @@ pp directors_database
 def print_first_directors_movie_titles
   director = directors_database
   string = ""
-  
+
   director[0][:movies][0].each do |title, name|
     if director[0][:movies][0][title] == title
       string += "#{name}\n"
     end
-  
+
   end
   string
 end
